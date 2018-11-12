99ed1591aa009db4a717f02830cd3516c1b32272
AYUMU <e185747@ie.u-ryukyu.ac.jp>
2018-11-12 13:49:56 +0900

    1st Commit

--- src/test/jp/ac/uryukyu/ie/tnal/EnemyTest.java
+++ src/test/jp/ac/uryukyu/ie/tnal/EnemyTest.java
@@ -8,9 +8,13 @@ class EnemyTest {
 
     @Test
     public void attack() {
+
         int heroHP = 10;
+
+        //ヒーローとエネミーの作成
         Hero hero = new Hero("テスト勇者", heroHP, 5);
         Enemy enemy = new Enemy("テストスライム", 6, 3);
+
         enemy.dead = true;
         for(int i=0; i<10; i++) {
             enemy.attack(hero); //乱数で0ダメージとなることもあるため、複数回実行してみる。
